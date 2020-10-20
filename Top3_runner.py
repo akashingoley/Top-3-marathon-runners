ec0b19f04aca1c71348f2ff36708ff7140b57970
Akash Ingoley <63150753+akashingoley@users.noreply.github.com>
2020-10-20 22:46:09 +0530

    The atom file of the code.
    file has been added to the repo

---
+++ Top3_runner.py
@@ -0,0 +1,20 @@
+distances = []
+while True:
+    intake = input("Enter the distances covered in kms : ")
+    if intake == "":
+        break
+    while float(intake) > 42 or float(intake) < 0:
+         print("Invalid distance \nValue should be between 0 and 42")
+         intake = input("Please enter the distance again : ")
+         if intake == "":
+             break
+    if intake == "":
+        break
+    distances.append(float(intake))
+    for i in distances:
+        if i == 42.00:
+            distances.remove(i)
+
+distances.sort(reverse = True)
+
+print (distances[:3])
